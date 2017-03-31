ed7942ba895c03ede5b4b2de5d6beb5fa04fa7f6
Dinesh Katwal <medineshkatwal@gmail.com>
2017-04-01 00:59:43 +0545

    Successfully config the facebook sdk in android

--- android/app/src/main/java/com/chatapp/MainApplication.java
+++ android/app/src/main/java/com/chatapp/MainApplication.java
@@ -9,34 +9,45 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+     private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+     protected static CallbackManager getCallbackManager() {
+       return mCallbackManager;
+     }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new FBSDKPackage()
-      );
-    }
-  };
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new FBSDKPackage(mCallbackManager)
+            );
+        }
+    };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // If you want to use AppEventsLogger to log events.
+        AppEventsLogger.activateApp(this);
+    }
 }
